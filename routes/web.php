1905c6ddbd5bba3243337c521b94cb6d7141218a
Yue <2429997028@qq.com>
2019-09-24 11:25:38 +0800

    xiugai

--- routes/web.php
+++ routes/web.php
@@ -31,6 +31,10 @@ Route::get('/index/teacher','Index\TeacherController@teacher');
 //后台
 Route::get('/admin/index','Admin\IndexController@index');
 
+
+Route::get('/admin/index','Admin\IndexController@index');
+Route::get('/admin/index','Admin\IndexController@index');
+
 Route::get('/admin/courseAdd','Admin\CourseController@courseAdd');// 课程添加页面
 Route::get('/admin/courseCategoryAdd','Admin\CourseController@courseCategoryAdd');//课程分类的添加页面
 Route::post('/admin/courseCategoryAdd_do','Admin\CourseController@courseCategoryAdd_do');//课程分类的添加执行
