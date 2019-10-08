82b2a4756abe63e70c8ac10043ace50d2ceed29e
liruyi123 <1446535591@qq.com>
2019-10-08 11:06:55 +0800

    xg

--- app/Http/Controllers/Index/AdvisoryController.php
+++ app/Http/Controllers/Index/AdvisoryController.php
@@ -7,9 +7,6 @@ use App\Model\Course;
 use App\Model\Navcon;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
-use App\Model\Navcon;
-use App\Model\Consult;
-use App\Model\Course;
 
 class AdvisoryController extends Controller
 {
@@ -17,23 +14,11 @@ class AdvisoryController extends Controller
     //咨询页面
     public function article()
     {
-<<<<<<< Updated upstream
-        $where = [
-            'show'  =>  0,
-            'navbar_id' =>  1
-        ];
-        $nav = Navcon::where('is_show',0)->orderBy('weight','desc')->get();
-        $data = Consult::where('show',0)->orderBy('c_time','desc')->get();
-        $info = Consult::where($where)->limit(5)->get();
-        $cou = Course::where('c_is_show',1)->orderBy('cou_weight','desc')->limit(3)->get();
-        return view('index.article',compact('nav','data','info','cou'));
-=======
         $data = Navcon::get();
         $arr = Consult::get();
         $hotask = Consult::where(['navbar_id'=>2])->get();
         $course = Course::get();
         return view('index.article',compact('data','arr','hotask','course'));
->>>>>>> Stashed changes
     }
     //咨询详情页面
     public function articlelist()
